3202804caff65abd412c64ed68d5dc7a3daaa145
Krishna <krishna05564@gmail.com>
2019-11-04 03:24:30 +0530

    gg

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const requestIp = require('request-ip');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(requestIp.mw())
+
+app.use(function(req, res) {
+    const ip = req.clientIp;
+    res.end(ip);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs= require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.ip);
+  fs.appendFile("list",req.ip+"\n"+req.connection.remoteAddress+"\n",(err)=>{
+    if(err) throw err;
+    console.log("Saved")
+  });
+
   console.log(req.connection.remoteAddress);  
   res.render('index');
 });
